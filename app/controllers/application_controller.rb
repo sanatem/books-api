511182b3d25fde5ca93d57a2d1457623b0ec7747
Santiago Pravisani <santiagopravisani@gmail.com>
2019-09-29 20:42:45 -0300

    Error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+
+  rescue_from ::ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ::NameError, with: :error_occurred
+  rescue_from ::ActionController::RoutingError, with: :error_occurred
+
+  def record_not_found(exception)
+    render json: { error: "Couldn't find entity. Check your params." }.to_json, status: 404
+  end
+
+  def error_occurred(exception)
+    render json: { error: exception.message }.to_json, status: 500
+  end
+
 end
